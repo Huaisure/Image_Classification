76dd844cc5017c8b2c94dba029e440f5b660dfbe
Huaisure <hs-liu21@mails.tsinghua.edu.cn>
2024-05-29 20:54:20 +0800

    update data_loader

--- data_preparation.py
+++ data_preparation.py
@@ -1,10 +1,34 @@
 import os
 import pandas as pd
+from PIL import Image
 from torchvision import transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader,Dataset
 from torchvision.datasets import ImageFolder
 
-def get_data_loader(train_dir, val_dir, test_dir, batch_size=32):
+class CustomDataset(Dataset):
+    def __init__(self,root_dir,type,transform=None):
+        self.root_dir = root_dir
+        csv_dir = os.path.join(root_dir,type+"_data.csv")
+        self.annotations = pd.read_csv(csv_dir)
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.annotations)
+    
+    def __getitem__(self, index):
+        img_name = os.path.join(self.root_dir,"imgs",self.annotations.iloc[index,0])
+        image = Image.open(img_name)
+        label = self.annotations.iloc[index,1]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image,label
+
+def get_data_loader(type:str, batch_size=32):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.join(base_dir, 'data')
+
     # 训练集数据增强
     train_transform = transforms.Compose([
         transforms.Resize((150,150)),
@@ -21,14 +45,14 @@ def get_data_loader(train_dir, val_dir, test_dir, batch_size=32):
     ])
 
     # 加载数据集
-    train_dataset = ImageFolder(train_dir, transform=train_transform)
-    val_dataset = ImageFolder(val_dir, transform=val_transform)
-    test_dataset = ImageFolder(test_dir, transform=val_transform)
+    if type == "train":
+        train_dataset = CustomDataset(data_dir,type,transform=train_transform)
+        val_dataset = CustomDataset(data_dir,"val",transform=val_transform)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-    return train_loader, val_loader, test_loader
-    
-    
+        return train_loader, val_loader
+    else:
+        test_dataset = CustomDataset(data_dir,type,transform=val_transform)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+        return test_loader
